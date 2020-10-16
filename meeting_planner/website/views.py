2fe9e09f8c108a5e98614838bf508560416a0b17
jedrickj@codev.com <jedrickj@codev.com>
2020-10-16 14:24:13 +0800

    First push

---
+++ meeting_planner/website/views.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from django.http import HttpResponse
+
+from django.shortcuts import render
+
+from meetings.models import Meeting
+
+
+def welcome(request):
+    return render(request, "website/welcome.html",
+                  {"meetings": Meeting.objects.all()})
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+def about(request):
+    return HttpResponse("I am Jedrick James Anthony")
